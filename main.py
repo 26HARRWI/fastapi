9cadbabacccbad3aa5c6a4587379122588ced906
William Harrison <133851767+26HARRWI@users.noreply.github.com>
2023-05-24 15:21:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,20 +3,16 @@ import uvicorn
 
 app = FastAPI()
 
-
 @app.get("/")
 def root():
     return {"endpoints": ["/get", "/post"]}
-
 
 @app.get("/get")
 def get():
     return {"message": "Hello World", "method": "GET"}
 
-
 @app.post("/post")
 def post():
     return {"message": "Hello World", "method": "POST"}
-
 
 uvicorn.run(app, host="0.0.0.0", port=8080)
